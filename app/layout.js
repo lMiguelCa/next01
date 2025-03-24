9e90ac2720226c08bdbb2ce9f0b6801a73cafa01
lMiguelCa <luis_castro15@usmp.pe>
2025-03-24 12:15:33 -0500

    layout para otras  ventanas

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,7 @@ import NavbarPage from '../components/Navbar';
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      
       <body>
         <NavbarPage/>
         {children}
---
+++ app/tienda/layout.js
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+
+export default function TiendaLayout({children}){
+    return (<>
+
+            <nav>
+                <h1>Seccion Tienda</h1>
+
+                <ul>
+                    <li><Link href="/tienda/categoria">Categoria</Link></li>
+                    <li><Link href="/tienda/categoria/computadoras">Productos</Link></li>
+
+                </ul>
+            </nav>
+
+        {children}
+        </>
+    );
+}
