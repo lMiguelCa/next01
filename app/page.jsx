f1907e2ac26849026ac32b53286bf4c199152ae8
lMiguelCa <luis_castro15@usmp.pe>
2025-03-26 11:56:22 -0500

    componente servidor

--- app/page.jsx
+++ app/page.jsx
@@ -1,22 +1,26 @@
 /*"use client"*/
 /*"use client"
 import { useState } from "react"*/
-"use client"
+/*"use client"*/
 
 import Users from "../components/Users"
-
+export const metadata ={
+    title: 'Mi pagina especial HOme'
+}
 export default function HomePage(){
+    //server componente
     return <section>
-        <h1>Hola shupapi</h1>
+        {/*<h1>Hola shupapi</h1>*/} 
 
         {/*<button onClick={()=> {
             alert('Hola')
        }}>          Click
         </button>*/} 
 
-        {
+        {/*{
             window.localStorage.getItem('token')
-        }
-        <Users/>
+        }*/} 
+        <Users/> {/* clien componente, cliente componente*/} 
     </section>
 }
+
