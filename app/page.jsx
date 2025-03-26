607bc3c77bfb503a6cc5dd3837c476a55bfb4d45
lMiguelCa <luis_castro15@usmp.pe>
2025-03-26 11:23:02 -0500

    componente con estado

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,14 @@
+/*"use client"*/
 "use client"
-
+import { useState } from "react"
 
 export default function HomePage(){
     return <section>
         <h1>Hola shupapi</h1>
-        <button onClick={()=> {
+
+        {/*<button onClick={()=> {
             alert('Hola')
        }}>          Click
-        </button> 
+        </button>*/} 
     </section>
 }
