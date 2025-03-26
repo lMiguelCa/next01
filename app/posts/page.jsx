7f10c294456ae65511388f5095e9de42fec7807b
lMiguelCa <luis_castro15@usmp.pe>
2025-03-26 12:45:18 -0500

    RSC mas RCC

--- app/posts/page.jsx
+++ app/posts/page.jsx
@@ -1,10 +1,13 @@
+
+import PostCard from '../../components/PostCard'
+
 async function loadPosts() {
     const res = await fetch ("https://jsonplaceholder.typicode.com/posts");
     const data = await res.json();
     return data;
 }
 
-//renderiza
+//RSC
 async function PostPages(){
     const posts = await loadPosts();
     console.log(posts);
@@ -12,10 +15,8 @@ async function PostPages(){
     return(
         <div>
             {posts.map((post)=>(
-                <div key={post.id}>
-                    <h3>{post.id}. {post.title}</h3>
-                    <p>{post.body}</p>
-                </div>
+                <PostCard post={post} key={post.id} />
+
             ))}
         </div>
     )
